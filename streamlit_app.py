bb684c52281154788764ff9c62526e4ae96343f7
carolineprogram <111787856+carolineprogram@users.noreply.github.com>
2025-07-27 11:58:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+st.title("☕️Koffie!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+data = pd.DataFrame({
+    'Naam': ['1', 'Bob', 'Charlie'],
+    'Gekocht': [25, 30, 35],
+    'Favours': [85, 90, 78],
+    'Brew': ['', '', ''],
+})
+st.table(data)
+    
