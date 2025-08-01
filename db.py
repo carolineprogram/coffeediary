8088477f9f75767f380b0acf004db00a2c1df0b8
carolineprogram <111787856+carolineprogram@users.noreply.github.com>
2025-08-01 15:37:35 +0200

    Update db.py

--- db.py
+++ db.py
@@ -40,9 +40,8 @@ def run_query(query, table, data=None, where=None, order=None):
             result = q.execute()
             
         elif query == "joinselect":
-        result = conn.table('koffie_soort').select('naam, koffie_winkel(waardegekocht)').execute()
-        return result
-
+            result = conn.table('koffie_soort').select('naam, koffie_winkel(waardegekocht)').execute()
+            
         elif query == "insert":
             result = q.insert(data).execute()
 
