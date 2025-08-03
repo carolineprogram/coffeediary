17f04705fd75d80824da9faff7aec06ffb85ccfc
carolineprogram <111787856+carolineprogram@users.noreply.github.com>
2025-08-03 12:14:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def get_koffie():
     #FROM koffie_soort as ks
     #LEFT JOIN koffie_winkel kw ON ks.id_winkel = kw.id
     #LEFT JOIN koffie_soort_flavours kfs ON ks.id = kfs.id_soort LEFT JOIN koffie_flavours kf ON kfs.id_flavour = kf.id
-    return run_query("select", "koffie_soort", ["naam", "koffie_winkel(waargekocht)", "koffie_soort_flavours!inner(koffie_flavours(flavour))"])
+    return run_query("select", "koffie_soort", ["naam", "koffie_winkel(waargekocht)", "koffie_soort_flavours(koffie_flavours(flavour))"])
 
 
 koffie = get_koffie()
